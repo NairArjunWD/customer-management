b5a624004a5d841af34b2148748745e233338e97
Arjun Nair <nair.arjun47@yahoo.com>
2020-05-04 21:11:41 -0700

    Create snippets

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -8,7 +8,18 @@ namespace ACM.BL
 {
     public class Customer
     {
+        public int CustomerId { get; private set; } 
+        public string EmailAddress { get; set; }
+        
         public string FirstName {get; set;}
+        
+        public string FullName
+        {
+            get
+            {
+                return LastName + "," + FirstName;
+            }
+        }
 
         private string _lastName;
         public string LastName
