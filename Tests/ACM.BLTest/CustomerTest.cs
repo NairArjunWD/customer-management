8632a9c7b02a215d0ab6eadfea92d69f14e9d29d
Arjun Nair <nair.arjun47@yahoo.com>
2020-05-05 22:01:03 -0700

    Test successful

---
+++ Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    public class CustomerTest
+    {
+        [Fact]
+        public void FullNameTestValid()
+        {
+            // Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins"
+            };
+            string expected = "Baggins, Bilbo";
+
+            // Act
+            string actual = customer.FullName;
+
+            // Assert
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
