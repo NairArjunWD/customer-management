e3110ae0ecc70fb8d68f6ec8b5857a444e15ab13
Arjun Nair <nair.arjun47@yahoo.com>
2020-05-07 23:15:30 -0700

    Add order repo

--- ACM.BL/Order.cs
+++ ACM.BL/Order.cs
@@ -17,22 +17,6 @@ namespace ACM.BL
         public DateTimeOffset? OrderDate { get; set; }
         public int OrderId { get; private set; }
 
-        // Retrieve one order.
-        public Order Retrieve(int orderID)
-        {
-            // Code that retrieves the defined order
-
-            return new Order();
-        }
-
-        // Saves the current order
-        public bool Saves()
-        {
-            // Code that saves the defined order
-
-            return true;
-        }
-
         // Validates the order data.
         public bool Validate()
         {
