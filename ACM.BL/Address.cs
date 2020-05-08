2e640cfcca02a9f0b40d1b7d1daf8c873dd4337f
Arjun Nair <nair.arjun47@yahoo.com>
2020-05-07 21:56:12 -0700

    Add Address cs

---
+++ ACM.BL/Address.cs
@@ -0,0 +1,34 @@
+namespace ACM.BL
+{
+    public class Address
+    {
+        public Address()
+        {
+
+        }
+        public Address(int addressID)
+        {
+            addressID = addressID;
+        }
+
+        public int AddressId { get; private set; }
+        public int AddressType { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string StreetLine1 { get; set; }
+        public string StreetLine2 { get; set; }
+
+        // Validate the address data.
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (PostalCode == null) isValid = false;
+
+            return isValid;
+        }
+
+    }
+}
