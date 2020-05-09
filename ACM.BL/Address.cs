236802cd432b881378b9bafc028c5d5fc94b1676
Arjun Nair <nair.arjun47@yahoo.com>
2020-05-08 23:04:54 -0700

    Add missing state method to object in address

--- ACM.BL/Address.cs
+++ ACM.BL/Address.cs
@@ -14,6 +14,7 @@ namespace ACM.BL
         public int AddressId { get; private set; }
         public int AddressType { get; set; }
         public string City { get; set; }
+        public string State { get; set; }
         public string Country { get; set; }
         public string PostalCode { get; set; }
         public string StreetLine1 { get; set; }
